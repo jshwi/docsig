85b32b02e8d02270d2b337288cedd5a50f8e22fb
jshwi <stephen@jshwisolutions.com>
2022-06-26 18:46:52 +1000

    add: Adds codes to printed messages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 [Unreleased](https://github.com/jshwi/docsig/compare/v0.9.0...HEAD)
 ------------------------------------------------------------------------
+### Added
+- Adds codes to printed messages
 
 [0.9.0](https://github.com/jshwi/docsig/releases/tag/v0.9.0) - 2022-06-26
 ------------------------------------------------------------------------
--- docsig/messages.py
+++ docsig/messages.py
@@ -8,13 +8,13 @@ W101 = "{module}::{func} is missing a docstring"
 
 #: Exxx: Error
 #: E1xx: Docstring
-E101 = "parameters out of order"
-E102 = "includes parameters that do not exist"
-E103 = "parameters missing"
-E104 = "return statement documented for None"
-E105 = "return missing from docstring"
-E106 = "duplicate parameters found"
-E107 = "parameter appears to be incorrectly documented"
+E101 = "E101: parameters out of order"
+E102 = "E102: includes parameters that do not exist"
+E103 = "E103: parameters missing"
+E104 = "E104: return statement documented for None"
+E105 = "E105: return missing from docstring"
+E106 = "E106: duplicate parameters found"
+E107 = "E107: parameter appears to be incorrectly documented"
 
 #: Hxxx: Hint
 #: H1xx: Docstring
