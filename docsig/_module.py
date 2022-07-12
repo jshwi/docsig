87d019ed3271bac7021f6f2a3b5db712090749f7
jshwi <stephen@jshwisolutions.com>
2022-07-12 13:29:17 +1000

    refactor: Refactors signature for modules

--- docsig/_module.py
+++ docsig/_module.py
@@ -22,12 +22,12 @@ class Parent(_MutableSequence[_Function]):
 
     def __init__(
         self,
-        path: _Path,
         node: _ast.Module | _ast.ClassDef,
+        path: _Path | None = None,
         method: bool = False,
     ) -> None:
         super().__init__()
-        self._name = f"{path}::"
+        self._name = f"{path}::" if path is not None else ""
         for subnode in node.body:
             if isinstance(subnode, _ast.FunctionDef) and not str(
                 subnode.name
@@ -52,12 +52,12 @@ class Parent(_MutableSequence[_Function]):
 class Class(Parent):  # pylint: disable=too-few-public-methods
     """Represents a class with method parameters.
 
-    :param path: Path that node is parsed from.
     :param node: Abstract syntax tree.
+    :param path: Path that node is parsed from.
     """
 
-    def __init__(self, path: _Path, node: _ast.ClassDef) -> None:
-        super().__init__(path, node, method=True)
+    def __init__(self, node: _ast.ClassDef, path: _Path | None = None) -> None:
+        super().__init__(node, path, method=True)
         self._name = f"{self._name}{node.name}::"
 
 
@@ -67,15 +67,14 @@ class Module(_MutableSequence[Parent]):
     :param path: Path to compile function data from.
     """
 
-    def __init__(self, path: _Path) -> None:
+    def __init__(self, node: _ast.Module, path: _Path | None = None) -> None:
         super().__init__()
-        node = _ast.parse(path.read_text())
-        self.append(Parent(path, node))
+        self.append(Parent(node, path))
         for subnode in node.body:
             if isinstance(subnode, _ast.ClassDef) and not str(
                 subnode.name
             ).startswith("_"):
-                self.append(Class(path, subnode))
+                self.append(Class(subnode, path))
 
 
 class Modules(_MutableSequence[Module]):
@@ -91,7 +90,7 @@ class Modules(_MutableSequence[Module]):
 
     def _populate(self, root: _Path) -> None:
         if root.is_file() and root.name.endswith(".py"):
-            self.append(Module(root))
+            self.append(Module(_ast.parse(root.read_text()), root))
 
         if root.is_dir():
             for path in root.iterdir():
