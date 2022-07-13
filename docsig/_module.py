cecc699b0f44f1fc15ef71898b10da24ba8acb3f
jshwi <stephen@jshwisolutions.com>
2022-07-13 13:23:35 +1000

    refactor: Adds `Parent.name`

--- docsig/_module.py
+++ docsig/_module.py
@@ -27,6 +27,7 @@ class Parent(_MutableSequence[_Function]):
         method: bool = False,
     ) -> None:
         super().__init__()
+        self._name = node.name
         self._path = f"{path}::" if path is not None else ""
         for subnode in node.body:
             if isinstance(subnode, _ast.FunctionDef) and not str(
@@ -44,6 +45,11 @@ class Parent(_MutableSequence[_Function]):
                     self.append(_Function(subnode, method))
 
     @property
+    def name(self) -> str:
+        """Name of parent."""
+        return self._name
+
+    @property
     def path(self) -> str:
         """Representation of path to parent."""
         return self._path
@@ -58,7 +64,8 @@ class Class(Parent):
 
     def __init__(self, node: _ast.ClassDef, path: _Path | None = None) -> None:
         super().__init__(node, path, method=True)
-        self._path = f"{self._path}{node.name}::"
+        self._name = node.name
+        self._path = f"{self._path}{self.name}::"
 
 
 class Module(_MutableSequence[Parent]):
