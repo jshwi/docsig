9c06c6d8efc33959e612d6de73e90dcb0c4ea9a6
jshwi <stephen@jshwisolutions.com>
2022-11-28 09:57:54 +1100

    Bump version: 0.24.1 → 0.25.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "0.24.1"
+release = "0.25.0"
 
 
 # -- General configuration ---------------------------------------------------
