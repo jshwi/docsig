a6da0b51eae633bc60ccc6fbba13f179b6491714
jshwi <stephen@jshwisolutions.com>
2022-11-28 12:47:38 +1100

    add: Add `messages` to `__all__`

--- docsig/__init__.py
+++ docsig/__init__.py
@@ -1,6 +1,7 @@
 """Check signature params for proper documentation."""
+from . import messages
 from ._core import docsig
 from ._main import main
 from ._version import __version__
 
-__all__ = ["__version__", "docsig", "main"]
+__all__ = ["__version__", "docsig", "main", "messages"]
