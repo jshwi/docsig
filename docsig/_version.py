9987642bf5832d08a0f9978276cbd6d63f3ccd35
jshwi <stephen@jshwisolutions.com>
2022-12-11 13:55:19 +1100

    Bump version: 0.30.1 → 0.31.0

--- docsig/_version.py
+++ docsig/_version.py
@@ -7,4 +7,4 @@ Contains the version of this package.
 Allows for access to the version internally without cyclic imports
 caused by accessing it through __init__.
 """
-__version__ = "0.30.1"
+__version__ = "0.31.0"
