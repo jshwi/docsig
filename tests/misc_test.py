e6797cfa705d7cb8eb59530cf2a7358b9a729282
Stephen Whitlock <stephen@jshwisolutions.com>
2024-04-21 21:15:01 +1000

    test: add separate file for exclude tests
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -341,38 +341,6 @@ def test_list_checks(
     assert all(i.code in std.out for i in E.values())
 
 
-def test_exclude_argument(
-    init_file: InitFileFixtureType,
-    main: MockMainType,
-) -> None:
-    """Test bash script is ignored when exclude argument passed.
-
-    :param init_file: Initialize a test file.
-    :param main: Mock ``main`` function.
-    """
-    template = """
-#!/usr/bin/env bash
-new-ssl() {
-  domain="${1}"
-  config="${2:-"${domain}.conf"}"
-  openssl \
-    req \
-    -new \
-    -newkey \
-    rsa:2048 \
-    -nodes \
-    -sha256 \
-    -out "${domain}.csr" \
-    -keyout "${domain}.key" \
-    -config "${config}"
-}
-
-new-ssl "${@}"
-"""
-    init_file(template)
-    assert main(".", long.exclude, "file.py") == 0
-
-
 def test_bad_py_file(
     init_file: InitFileFixtureType,
     main: MockMainType,
@@ -406,23 +374,6 @@ new-ssl "${@}"
         main(".")
 
     assert "invalid syntax" in str(err.value)
-
-
-def test_exclude_defaults(
-    init_file: InitFileFixtureType,
-    main: MockMainType,
-) -> None:
-    """Test bash script is ignored when under __pycache__ directory.
-
-    :param init_file: Initialize a test file.
-    :param main: Mock ``main`` function.
-    """
-    template = """\
-#!/usr/bin/env bash
-echo "${1//[ ]/\\\\ }"
-"""
-    init_file(template, Path("__pycache__") / "file.py")
-    assert main(".") == 0
 
 
 def test_bash_script(
