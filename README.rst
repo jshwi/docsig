99899886d27e8552da2e58aa560ce8e4e28d3485
Stephen Whitlock <stephen@jshwisolutions.com>
2024-05-13 07:31:28 +1000

    bump: version 0.53.2 → 0.53.3
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- README.rst
+++ README.rst
@@ -194,7 +194,7 @@ It can be added to your .pre-commit-config.yaml as follows:
 
     repos:
       - repo: https://github.com/jshwi/docsig
-        rev: v0.53.2
+        rev: v0.53.3
         hooks:
           - id: docsig
             args:
--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Stephen Whitlock"
 author = "Stephen Whitlock"
 
 # The full version, including alpha/beta/rc tags
-release = "0.53.2"
+release = "0.53.3"
 
 
 # -- General configuration ---------------------------------------------------
