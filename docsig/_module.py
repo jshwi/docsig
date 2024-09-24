3dfc9714e3d4e13d7079a8c2eec3a1de1490d1cd
Stephen Whitlock <stephen@jshwisolutions.com>
2024-09-25 09:50:56 +1000

    refactor: add imports type
    Signed-off-by: Stephen Whitlock <stephen@jshwisolutions.com>

--- docsig/_module.py
+++ docsig/_module.py
@@ -20,6 +20,10 @@ from ._stub import Signature as _Signature
 from .messages import Messages as _Messages
 
 
+class _Imports(_t.Dict[str, str]):
+    """Represents python imports."""
+
+
 class Parent(_t.List["Parent"]):
     """Represents an object that contains functions or methods.
 
@@ -43,12 +47,12 @@ class Parent(_t.List["Parent"]):
         ignore_args: bool = False,
         ignore_kwargs: bool = False,
         check_class_constructor: bool = False,
-        imports: dict[str, str] | None = None,
+        imports: _Imports | None = None,
     ) -> None:
         super().__init__()
         self._name = node.name
         self._overloads = _Overloads()
-        self._imports = imports or {}
+        self._imports = imports or _Imports()
         self._parse_ast(
             node,
             directives,
@@ -167,7 +171,7 @@ class Function(Parent):
         ignore_args: bool = False,
         ignore_kwargs: bool = False,
         check_class_constructor: bool = False,
-        imports: dict[str, str] | None = None,
+        imports: _Imports | None = None,
     ) -> None:
         super().__init__(
             node,
