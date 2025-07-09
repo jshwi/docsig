0c98310eefd6dc1bb1873b574994986830632f6d
Stephen Whitlock <s.whitlock@live.com>
2025-07-09 21:45:12 +1000

    wip: deprecate some short form options
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_config.py
+++ docsig/_config.py
@@ -8,7 +8,9 @@ from __future__ import annotations as _
 import argparse as _a
 import os as _os
 import re as _re
+import sys as _sys
 import typing as _t
+import warnings as _warnings
 from pathlib import Path as _Path
 
 import tomli as _tomli
@@ -89,12 +91,48 @@ class _ArgumentParser(_a.ArgumentParser):
         return namespace, args
 
 
+def _warn_on_deprecated_short_flags():
+    deprecated_short_flags = {
+        "-c": "--check-class",
+        "-C": "--check-class-constructor",
+        "-D": "--check-dunders",
+        "-m": "--check-protected-class-methods",
+        "-N": "--check-nested",
+        "-o": "--check-overridden",
+        "-p": "--check-protected",
+        "-P": "--check-property-returns",
+        "-U": "--enforce-capitalization",
+        "-i": "--ignore-no-params",
+        "-a": "--ignore-args",
+        "-k": "--ignore-kwargs",
+        "-T": "--ignore-typechecker",
+    }
+    raw_args = _sys.argv[1:]
+    expanded_flags = []
+    for arg in raw_args:
+        if arg.startswith("--") or not arg.startswith("-") or arg == "-":
+            expanded_flags.append(arg)
+        elif len(arg) > 2:
+            expanded_flags.extend([f"-{ch}" for ch in arg[1:]])
+        else:
+            expanded_flags.append(arg)
+
+    used_flags = set(expanded_flags)
+    for short, long in deprecated_short_flags.items():
+        if short in used_flags:
+            _warnings.warn(
+                f"short option '{short}' is deprecated, use '{long}' instead",
+                stacklevel=2,
+            )
+
+
 def parse_args(args: _t.Sequence[str] | None = None) -> _a.Namespace:
     """Parse commandline arguments.
 
     :param args: Args for manual parsing.
     :return: Parsed arguments.
     """
+    _warn_on_deprecated_short_flags()
     parser = _ArgumentParser(
         description="Check signature params for proper documentation",
     )
@@ -132,81 +170,159 @@ def parse_args(args: _t.Sequence[str] | None = None) -> _a.Namespace:
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument(
         "-c",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_class",
+    )
+    group.add_argument(
         "--check-class",
         action="store_true",
         help="check class docstrings",
+        dest="check_class",
     )
     group.add_argument(
         "-C",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_class_constructor",
+    )
+    group.add_argument(
         "--check-class-constructor",
         action="store_true",
-        help="check __init__ methods (mutually exclusive with --class-class)",
+        help="check __init__ methods (mutually exclusive with --check-class)",
+        dest="check_class_constructor",
     )
     parser.add_argument(
         "-D",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_dunders",
+    )
+    parser.add_argument(
         "--check-dunders",
         action="store_true",
         help="check dunder methods",
+        dest="check_dunders",
     )
     parser.add_argument(
         "-N",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_nested",
+    )
+    parser.add_argument(
         "--check-nested",
         action="store_true",
         help="check nested functions and classes",
+        dest="check_nested",
     )
     parser.add_argument(
         "-o",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_overridden",
+    )
+    parser.add_argument(
         "--check-overridden",
         action="store_true",
         help="check overridden methods",
+        dest="check_overridden",
     )
     parser.add_argument(
         "-P",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_property_returns",
+    )
+    parser.add_argument(
         "--check-property-returns",
         action="store_true",
         help="check property return values",
+        dest="check_property_returns",
     )
     parser.add_argument(
         "-p",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_protected",
+    )
+    parser.add_argument(
         "--check-protected",
         action="store_true",
         help="check protected functions and classes",
+        dest="check_protected",
     )
     parser.add_argument(
         "-m",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="check_protected_class_methods",
+    )
+    parser.add_argument(
         "--check-protected-class-methods",
         action="store_true",
         help="check public methods belonging to protected classes",
+        dest="check_protected_class_methods",
     )
     parser.add_argument(
         "-U",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="enforce_capitalization",
+    )
+    parser.add_argument(
         "--enforce-capitalization",
         action="store_true",
         help="ensure param descriptions are capitalized",
+        dest="enforce_capitalization",
     )
     parser.add_argument(
         "-a",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="ignore_args",
+    )
+    parser.add_argument(
         "--ignore-args",
         action="store_true",
         help="ignore args prefixed with an asterisk",
+        dest="ignore_args",
     )
     parser.add_argument(
         "-k",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="ignore_kwargs",
+    )
+    parser.add_argument(
         "--ignore-kwargs",
         action="store_true",
         help="ignore kwargs prefixed with two asterisks",
+        dest="ignore_kwargs",
     )
     parser.add_argument(
         "-i",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="ignore_no_params",
+    )
+    parser.add_argument(
         "--ignore-no-params",
         action="store_true",
         help="ignore docstrings where parameters are not documented",
+        dest="ignore_no_params",
     )
     parser.add_argument(
         "-T",
+        action="store_true",
+        help=_a.SUPPRESS,
+        dest="ignore_typechecker",
+    )
+    parser.add_argument(
         "--ignore-typechecker",
         action="store_true",
         help="ignore checking return values",
+        dest="ignore_typechecker",
     )
     parser.add_argument(
         "-d",
