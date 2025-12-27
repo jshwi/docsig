79c6f60c0d53eae2d68ae8e115d2ced5cfaaf4eb
Stephen Whitlock <s.whitlock@live.com>
2025-12-27 22:12:21 +1100

    chore: fix tox implementation
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

---
+++ scripts/tox.sh
@@ -0,0 +1,10 @@
+set -euo pipefail
+src="${1}"
+dst="${2}"
+rm -rf "${dst}"
+git clone --no-hardlinks "${src}" "${dst}" >/dev/null 2>&1
+cd "${dst}"
+unset VIRTUAL_ENV
+unset POETRY_ACTIVE
+hash -r 2>/dev/null || true
+make build
