f24b937009db06e3cac41500d23a6d386821a1fa
Stephen Whitlock <s.whitlock@live.com>
2026-01-27 14:40:03 +1100

    wip: remove short form for check-overridden
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/_test.py
+++ tests/_test.py
@@ -945,7 +945,7 @@ def function(a, b) -> None:
 """
     make_tree({"module": {"file.py": [template]}})
     with pytest.warns(FutureWarning):
-        main(".", "-cDoIv", test_flake8=False)
+        main(".", "-cDIv", test_flake8=False)
 
 
 def test_ignore_no_params(
