060cb22f58bf541f99755a6077039bedb985c486
Stephen Whitlock <s.whitlock@live.com>
2026-01-27 14:53:19 +1100

    wip: remove short form for check-class
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -906,21 +906,3 @@ class Klass:
     std = capsys.readouterr()
     assert E[505].ref in std.out
     assert main(".", "--ignore-typechecker") == 0
-
-
-def test_compressed_short_form_warning(
-    make_tree: FixtureMakeTree,
-    main: FixtureMain,
-) -> None:
-    """Test warnings for short form options.
-
-    :param make_tree: Create the directory tree from dict mapping.
-    :param main: Mock ``main`` function.
-    """
-    template = """\
-def function(a, b) -> None:
-    pass
-"""
-    make_tree({"module": {"file.py": [template]}})
-    with pytest.warns(FutureWarning):
-        main(".", "-cI", test_flake8=False)
