ce48a36d3993e843651a9a90518f5ab5f89fb430
Stephen Whitlock <s.whitlock@live.com>
2026-01-27 14:53:19 +1100

    wip: remove short form for check-class
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_main.py
+++ docsig/_main.py
@@ -17,7 +17,6 @@ from ._hooks import excepthook as _excepthook
 def _warn_on_deprecated_short_flags() -> None:
     deprecated_short_flags = {
         "-I": "--include-ignored",
-        "-c": "--check-class",
     }
     raw_args = _sys.argv[1:]
     expanded_flags = []
