53cf6ae8e6f2d5945cb3e79cf6801558ee539178
Stephen Whitlock <s.whitlock@live.com>
2026-01-27 14:38:11 +1100

    wip: remove short form for check-protected
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -957,4 +957,4 @@ def function(a, b) -> None:
 """
     make_tree({"module": {"file.py": [template]}})
     with pytest.warns(FutureWarning):
-        main(".", "-cDopIv", test_flake8=False)
+        main(".", "-cDoIv", test_flake8=False)
