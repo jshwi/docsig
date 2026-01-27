e6961e69ecfcf32755c624c24edb930b338dc16b
Stephen Whitlock <s.whitlock@live.com>
2026-01-27 14:36:58 +1100

    wip: remove short form for check-property-returns
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -957,4 +957,4 @@ def function(a, b) -> None:
 """
     make_tree({"module": {"file.py": [template]}})
     with pytest.warns(FutureWarning):
-        main(".", "-cDopPIv", test_flake8=False)
+        main(".", "-cDopIv", test_flake8=False)
