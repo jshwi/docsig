817822cda86d2ad91c265f46a30e6bb88c4c749a
Stephen Whitlock <s.whitlock@live.com>
2026-01-30 15:04:10 +1100

    refactor: fix parameter forwarding
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_main.py
+++ docsig/_main.py
@@ -59,27 +59,6 @@ def main() -> str | int:
     _warn_on_deprecated_short_flags()
     a = _parse_args()
     _excepthook(a.no_ansi)
-    return _docsig(
-        *a.path,
-        string=a.string,
-        list_checks=a.list_checks,
-        check_class=a.check_class,
-        check_class_constructor=a.check_class_constructor,
-        check_dunders=a.check_dunders,
-        check_protected_class_methods=a.check_protected_class_methods,
-        check_nested=a.check_nested,
-        check_overridden=a.check_overridden,
-        check_protected=a.check_protected,
-        check_property_returns=a.check_property_returns,
-        include_ignored=a.include_ignored,
-        ignore_no_params=a.ignore_no_params,
-        ignore_args=a.ignore_args,
-        ignore_kwargs=a.ignore_kwargs,
-        ignore_typechecker=a.ignore_typechecker,
-        no_ansi=a.no_ansi,
-        verbose=a.verbose,
-        target=a.target,
-        disable=a.disable,
-        exclude=a.exclude,
-        excludes=a.excludes,
-    )
+    kwargs = vars(a)
+    path = kwargs.pop("path")
+    return _docsig(*path, **kwargs)
