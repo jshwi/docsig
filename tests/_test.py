2496b36bb7507e76bab91ce024b725f192e4b83e
Stephen Whitlock <s.whitlock@live.com>
2026-03-04 18:46:56 +1100

    wip: remove short form for include-ignored
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/_test.py
+++ tests/_test.py
@@ -1041,24 +1041,6 @@ class Klass:
     )
 
 
-def test_compressed_short_form_warning(
-    make_tree: FixtureMakeTree,
-    main: FixtureMain,
-) -> None:
-    """Test warnings for short form options.
-
-    :param make_tree: Create the directory tree from dict mapping.
-    :param main: Mock ``main`` function.
-    """
-    template = """\
-def function(a, b) -> None:
-    pass
-"""
-    make_tree({"module": {"file.py": [template]}})
-    with pytest.warns(FutureWarning):
-        main(".", "-Iv", test_flake8=False)
-
-
 def test_ignore_no_params(
     init_file: FixtureInitFile,
     main: FixtureMain,
