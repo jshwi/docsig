1b528efb20401def5f8f3929b1489ac2a230191e
Stephen Whitlock <s.whitlock@live.com>
2026-03-04 18:46:56 +1100

    wip: remove short form for include-ignored
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_main.py
+++ docsig/_main.py
@@ -7,36 +7,9 @@ Contains package entry point.
 
 from __future__ import annotations as _
 
-import sys as _sys
-import warnings as _warnings
-
 from ._config import parse_args as _parse_args
 from ._core import docsig as _docsig
 from ._hooks import excepthook as _excepthook
-
-
-def _warn_on_deprecated_short_flags() -> None:
-    deprecated_short_flags = {
-        "-I": "--include-ignored",
-    }
-    raw_args = _sys.argv[1:]
-    expanded_flags = []
-    for arg in raw_args:
-        if arg.startswith("--") or not arg.startswith("-") or arg == "-":
-            expanded_flags.append(arg)
-        elif len(arg) > 2:
-            expanded_flags.extend([f"-{ch}" for ch in arg[1:]])
-        else:
-            expanded_flags.append(arg)
-
-    used_flags = set(expanded_flags)
-    for short, long in deprecated_short_flags.items():
-        if short in used_flags:
-            _warnings.warn(
-                f"short option '{short}' is deprecated, use '{long}' instead",
-                category=FutureWarning,
-                stacklevel=2,
-            )
 
 
 def main() -> str | int:
@@ -46,7 +19,6 @@ def main() -> str | int:
 
     :return: Exit status for whether the test failed or not.
     """
-    _warn_on_deprecated_short_flags()
     a = _parse_args()
     _excepthook(a.no_ansi)
     kwargs = vars(a)
