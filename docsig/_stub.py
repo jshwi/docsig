b1610fc3d1ce1175a781e343687f6f81715eec09
Stephen Whitlock <s.whitlock@live.com>
2026-03-04 21:32:38 +1100

    refactor: add properties
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_stub.py
+++ docsig/_stub.py
@@ -100,11 +100,11 @@ class Param:
         indent: int = 0,
         closing_token: str = ":",
     ) -> None:
-        self.kind = kind
-        self.name = name
-        self.description = description
-        self.indent = indent
-        self.closing_token = closing_token
+        self._kind = kind
+        self._name = name
+        self._description = description
+        self._indent = indent
+        self._closing_token = closing_token
 
     def __eq__(self, other: object) -> bool:
         iseq = False
@@ -124,6 +124,31 @@ class Param:
     def isprotected(self) -> bool:
         """True if the parameter name starts with an underscore."""
         return str(self.name).startswith("_")
+
+    @property
+    def kind(self) -> DocType:
+        """Type of the param."""
+        return self._kind
+
+    @property
+    def name(self) -> str | None:
+        """Name of the param."""
+        return self._name
+
+    @property
+    def description(self) -> str | None:
+        """Description of param."""
+        return self._description
+
+    @property
+    def indent(self) -> int:
+        """Number of spaces in the indent."""
+        return self._indent
+
+    @property
+    def closing_token(self) -> str:
+        """Token used to terminate the param name definition."""
+        return self._closing_token
 
 
 class Params(_t.List[Param]):
