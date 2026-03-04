a2bd3d17ae2edb1062f20702a49fb2794a2b9295
Stephen Whitlock <s.whitlock@live.com>
2026-03-04 19:45:46 +1100

    test: add permanent arg to test all compressed short form args
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -957,4 +957,4 @@ def function(a, b) -> None:
 """
     make_tree({"module": {"file.py": [template]}})
     with pytest.warns(FutureWarning):
-        main(".", "-cDopPI", test_flake8=False)
+        main(".", "-cDopPIv", test_flake8=False)
