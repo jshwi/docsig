fd84dd9fb6727b8b0c41b6611482f92661068618
Stephen Whitlock <s.whitlock@live.com>
2026-04-15 18:48:42 +1000

    doc(extension): use app in generate extension
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docs/extensions/generate.py
+++ docs/extensions/generate.py
@@ -1,32 +1,15 @@
 """Generate version of changelog suitable for documentation."""
 
-import functools
 import json
 import os
 import re
 import subprocess
 import typing as t
-from pathlib import Path
 
 import yaml
 from sphinx.application import Sphinx
 
 from docsig.plugin import ValidatePyproject
-
-DOCS_DIR = Path(__file__).parent.parent.absolute()
-GENERATED = DOCS_DIR / "_generated"
-GITIGNORE = GENERATED / ".gitignore"
-REPO = DOCS_DIR.parent
-README = REPO / "README.rst"
-TEST_RST = """
-tests
-=====
-
-.. automodule:: tests.base_test
-    :members:
-    :undoc-members:
-    :show-inheritance:
-"""
 
 
 def _normalize_strange_characters(s: str) -> str:
@@ -52,27 +35,13 @@ def code_block(lang: str) -> re.Pattern:
     return re.compile(rf"(\.\. code-block:: {lang}\s*\n(?:\s{{4}}.+\n)+)")
 
 
-def extension(func: t.Callable[..., None]) -> t.Callable[..., None]:
-    """Consume unused sphinx option.
-
-    :param func: Extension function.
-    :return: Decorated function.
-    """
-
-    @functools.wraps(func)
-    def wrapper(_: Sphinx) -> None:
-        return func()
-
-    return wrapper
-
-
-def get_docsig_help() -> t.Optional[str]:
+def get_docsig_help(app: Sphinx) -> t.Optional[str]:
     """Extract help from readme.
 
+    :param app: The Sphinx application to register the extension with.
     :return: Help text, or None (won't be None - typechecker stuff).
     """
-    content = README.read_text(encoding="utf-8")
-    items = code_block("console").findall(content)
+    items = code_block("console").findall(app.config.readme)
     for item in items:
         if "usage: docsig" in item:
             return item
@@ -80,78 +49,108 @@ def get_docsig_help() -> t.Optional[str]:
     return None
 
 
-@extension
-def make_generated_dir() -> None:
-    """Make dir for generated files and make sure it is ignored."""
-    GENERATED.mkdir(exist_ok=True, parents=True)
-    GITIGNORE.write_text("*", encoding="utf-8")
+def make_generated(app: Sphinx) -> None:
+    """Make dir for generated files and make sure it is ignored.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    app.config.generated.mkdir(exist_ok=True, parents=True)
+    (app.config.generated / ".gitignore").write_text("*", encoding="utf-8")
 
 
-@extension
-def generate_changelog() -> None:
-    """Generate changelog documentation."""
+def generate_changelog(app: Sphinx) -> None:
+    """Generate changelog documentation.
+
+    :param app: The Sphinx application to register the extension with.
+    """
     content = "# Changelog\n\n{}".format(
         (
-            (REPO / "CHANGELOG.md")
+            (app.config.repo / "CHANGELOG.md")
             .read_text(encoding="utf-8")
             .split("<!-- release notes start -->")
         )[1],
     )
-    (GENERATED / "changelog.md").write_text(content, encoding="utf-8")
-
-
-@extension
-def generate_contributing() -> None:
-    """Generate contributing documentation."""
-    content = (
-        (REPO / "CONTRIBUTING.md")
-        .read_text(encoding="utf-8")
-        .replace("# Contributing to docsig", "# Contributing")
-    )
-    (GENERATED / "contributing.md").write_text(content, encoding="utf-8")
-
-
-@extension
-def generate_docsig_help() -> None:
-    """Generate docsig help documentation."""
-    content = get_docsig_help()
-    if content is not None:
-        (GENERATED / "docsig-help.rst").write_text(content, encoding="utf-8")
-
-
-@extension
-def generate_pyproject_toml_example() -> None:
-    """Generate pyproject.toml example."""
-    content = README.read_text(encoding="utf-8")
-    (GENERATED / "pyproject-toml-example.rst").write_text(
-        code_block("toml").findall(content)[0],
+    (app.config.generated / "changelog.md").write_text(
+        content,
         encoding="utf-8",
     )
 
 
-@extension
-def generate_pyproject_toml_schema_info() -> None:
-    """Generate pyproject.toml schema info."""
-    content = README.read_text(encoding="utf-8")
+def generate_contributing(app: Sphinx) -> None:
+    """Generate contributing documentation.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    content = (
+        (app.config.repo / "CONTRIBUTING.md")
+        .read_text(encoding="utf-8")
+        .replace("# Contributing to docsig", "# Contributing")
+    )
+    (app.config.generated / "contributing.md").write_text(
+        content,
+        encoding="utf-8",
+    )
+
+
+def generate_docsig_help(app: Sphinx) -> None:
+    """Generate docsig help documentation.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    content = get_docsig_help(app)
+    if content is not None:
+        (app.config.generated / "docsig-help.rst").write_text(
+            content,
+            encoding="utf-8",
+        )
+
+
+def generate_pyproject_toml_example(app: Sphinx) -> None:
+    """Generate pyproject.toml example.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    (app.config.generated / "pyproject-toml-example.rst").write_text(
+        code_block("toml").findall(app.config.readme)[0],
+        encoding="utf-8",
+    )
+
+
+def generate_pyproject_toml_schema_info(app: Sphinx) -> None:
+    """Generate pyproject.toml schema info.
+
+    :param app: The Sphinx application to register the extension with.
+    """
     pattern = re.compile(
         r"\.\. schema-info start\s*(.*?)\s*\.\. schema-info end",
         re.DOTALL,
     )
-    match = pattern.search(content)
-    (GENERATED / "pyproject-schema-info.rst").write_text(
+    match = pattern.search(app.config.readme)
+    (app.config.generated / "pyproject-schema-info.rst").write_text(
         match.group(1).strip() if match else "",
         encoding="utf-8",
     )
 
 
-@extension
-def generate_tests() -> None:
-    """Generate documentation for tests."""
-    os.environ["PYTHONPATH"] = str(REPO)
+def generate_tests(app: Sphinx) -> None:
+    """Generate documentation for tests.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    os.environ["PYTHONPATH"] = str(app.config.repo)
     content = []
-    source = DOCS_DIR / "_build"
+    source = app.srcdir / "_build"
     build = source / "markdown"
-    (source / "index.rst").write_text(TEST_RST)
+    test_rst = """
+tests
+=====
+
+.. automodule:: tests.base_test
+    :members:
+    :undoc-members:
+    :show-inheritance:
+"""
+    (source / "index.rst").write_text(test_rst)
     subprocess.run(
         [
             "sphinx-build",
@@ -183,7 +182,7 @@ def generate_tests() -> None:
         elif not skip_lines:
             content.append(line)
 
-    (GENERATED / "tests.md").write_text(
+    (app.config.generated / "tests.md").write_text(
         _normalize_strange_characters(
             "{}\n".format(
                 "\n".join(content)
@@ -195,31 +194,35 @@ def generate_tests() -> None:
     )
 
 
-@extension
-def generate_pre_commit_example() -> None:
-    """Generate pre-commit example."""
-    content = README.read_text(encoding="utf-8")
-    (GENERATED / "pre-commit-example.rst").write_text(
-        code_block("yaml").findall(content)[0],
+def generate_pre_commit_example(app: Sphinx) -> None:
+    """Generate pre-commit example.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    (app.config.generated / "pre-commit-example.rst").write_text(
+        code_block("yaml").findall(app.config.readme)[0],
         encoding="utf-8",
     )
 
 
-@extension
-def generate_pre_commit_flake8_example() -> None:
-    """Generate pre-commit flake8 example."""
-    content = README.read_text(encoding="utf-8")
-    (GENERATED / "pre-commit-flake8-example.rst").write_text(
-        code_block("yaml").findall(content)[1],
+def generate_pre_commit_flake8_example(app: Sphinx) -> None:
+    """Generate pre-commit flake8 example.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    (app.config.generated / "pre-commit-flake8-example.rst").write_text(
+        code_block("yaml").findall(app.config.readme)[1],
         encoding="utf-8",
     )
 
 
-@extension
-def generate_commit_policy() -> None:
-    """Generate commit-policy page."""
-    file = REPO / ".conform.yaml"
-    build = GENERATED / "commit-policy.md"
+def generate_commit_policy(app: Sphinx) -> None:
+    """Generate commit-policy page.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    file = app.config.repo / ".conform.yaml"
+    build = app.config.generated / "commit-policy.md"
     content = ["# Commit Policy\n"]
     policy = yaml.safe_load(file.read_text(encoding="utf-8"))["policies"][0]
     for header, obj in policy["spec"].items():
@@ -246,21 +249,24 @@ def generate_commit_policy() -> None:
     build.write_text("\n".join(content), encoding="utf-8")
 
 
-@extension
-def generate_flake8_help() -> None:
-    """Generate flake8 documentation."""
-    build = GENERATED / "flake8.rst"
-    readme = README.read_text(encoding="utf-8")
+def generate_flake8_help(app: Sphinx) -> None:
+    """Generate flake8 documentation.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    build = app.config.generated / "flake8.rst"
     pattern = re.compile(r"Flake8\n\*{6}(.*?)end flake8", re.DOTALL)
-    match = pattern.search(readme)
+    match = pattern.search(app.config.readme)
     if match:
         build.write_text(match.group(1).strip()[:-2].strip(), encoding="utf-8")
 
 
-@extension
-def generate_schema() -> None:
-    """Generate docsig schema."""
-    (GENERATED / "schema.json").write_text(
+def generate_schema(app: Sphinx) -> None:
+    """Generate docsig schema.
+
+    :param app: The Sphinx application to register the extension with.
+    """
+    (app.config.generated / "schema.json").write_text(
         json.dumps(ValidatePyproject(), indent=2),
     )
 
@@ -270,12 +276,18 @@ def setup(app: Sphinx) -> None:
 
     :param app: The Sphinx application to register the extension with.
     """
-    app.connect("builder-inited", make_generated_dir)
+    app.add_config_value("generated", app.srcdir / "_generated", "html")
+    app.add_config_value("repo", app.srcdir.parent, "html")
+    app.add_config_value(
+        "readme",
+        (app.config.repo / "README.rst").read_text(encoding="utf-8"),
+        "html",
+    )
+    app.connect("builder-inited", make_generated)
     app.connect("builder-inited", generate_contributing)
     app.connect("builder-inited", generate_changelog)
     app.connect("builder-inited", generate_docsig_help)
     app.connect("builder-inited", generate_pyproject_toml_example)
-    app.connect("builder-inited", generate_pyproject_toml_schema_info)
     app.connect("builder-inited", generate_tests)
     app.connect("builder-inited", generate_pre_commit_example)
     app.connect("builder-inited", generate_pre_commit_flake8_example)
