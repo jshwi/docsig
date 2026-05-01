f24c27e539f4fc96e108c962c4c07feb764b4b16
Stephen Whitlock <s.whitlock@live.com>
2026-05-01 14:12:01 +1000

    chore(make): document version getter
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- scripts/get_docsig_version.sh
+++ scripts/get_docsig_version.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# use this instead of `$ poetry version --short` as the version may be
+# needed before poetry is installed
 grep -E '^version = ' pyproject.toml |
   sed 's/.*= *"\([^"]*\)".*/\1/' 2>/dev/null
