2640a3af22e07b82e9fc32bb88231a5e8bf7ccf9
Stephen Whitlock <s.whitlock@live.com>
2026-05-09 15:07:43 +1000

    chore(script): allow other files and unquoted versions
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- scripts/get_docsig_version.sh
+++ scripts/get_docsig_version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # use this instead of `$ poetry version --short` as the version may be
 # needed before poetry is installed
-grep -E '^version = ' pyproject.toml |
-  sed 's/.*= *"\([^"]*\)".*/\1/' 2>/dev/null
+set -u
+grep -E '^version *= *' "${1}" |
+  sed -E 's/.*= *"?([^"]*)"?/\1/' 2>/dev/null
