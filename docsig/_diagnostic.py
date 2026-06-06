6b9ad2884324c28ed6831ca795c56d8a7d80739e
Stephen Whitlock <s.whitlock@live.com>
2026-06-06 21:13:01 +1000

    refactor: move retcode to diagnostic module
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/_diagnostic.py
+++ docsig/_diagnostic.py
@@ -8,6 +8,28 @@ Diagnostic records and per-function check results for docsig runs.
 from dataclasses import dataclass as _dataclass
 
 
+class RetCode:
+    """RetCode object.
+
+    :param code: Initial return code, if any, otherwise zero.
+    """
+
+    def __init__(self, code: int = 0) -> None:
+        self._data = [code]
+
+    def add(self, code: int) -> None:
+        """Add a return code.
+
+        :param code: Return code to add.
+        """
+        self._data.append(code)
+
+    @property
+    def result(self) -> int:
+        """Maximum return code."""
+        return max(self._data)
+
+
 @_dataclass(frozen=True, order=True)
 class Diagnostic:  # pylint: disable=too-few-public-methods
     """Single reported issue for one function."""
