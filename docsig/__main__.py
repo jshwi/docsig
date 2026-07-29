af7909000dc40d08b43d141b901d8ba1ebada7b1
Stephen Whitlock <s.whitlock@live.com>
2026-07-29 20:19:27 +1000

    refactor: remove stale pragma no cover comments
    Signed-off-by: Stephen Whitlock <s.whitlock@live.com>

--- docsig/__main__.py
+++ docsig/__main__.py
@@ -9,5 +9,5 @@ import sys as _sys  # pragma: no cover
 
 from docsig import main  # pragma: no cover
 
-if __name__ == "__main__":  # pragma: no cover
-    _sys.exit(main())  # pragma: no cover
+if __name__ == "__main__":
+    _sys.exit(main())
